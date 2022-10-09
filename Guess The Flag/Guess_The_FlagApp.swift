b1eb14c8721b4a174d9898c7419468c44a6247e0
ajea <smithajea@rocketmail.com>
2022-10-09 16:22:53 -0400

    Initial Commit

---
+++ Guess The Flag/Guess_The_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_FlagApp.swift
+//  Guess The Flag
+//
+//  Created by Ajea Smith on 10/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
