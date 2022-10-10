d43533050b8052a3ef73f3f041a474207809a107
ajea <smithajea@rocketmail.com>
2022-10-10 15:59:11 -0400

    - Add Gradient file

---
+++ Guess The Flag/Concepts/Gradients.swift
@@ -0,0 +1,20 @@
+//
+//  Gradients.swift
+//  Guess The Flag
+//
+//  Created by Ajea Smith on 10/10/22.
+//
+
+import SwiftUI
+
+struct Gradients: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Gradients_Previews: PreviewProvider {
+    static var previews: some View {
+        Gradients()
+    }
+}
