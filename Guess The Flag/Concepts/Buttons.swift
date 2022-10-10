d754260e56b78ca3cc082d92c2b9a9c51f6a1e3b
ajea <smithajea@rocketmail.com>
2022-10-10 16:08:10 -0400

    - Add Buttons file

---
+++ Guess The Flag/Concepts/Buttons.swift
@@ -0,0 +1,20 @@
+//
+//  Buttons.swift
+//  Guess The Flag
+//
+//  Created by Ajea Smith on 10/10/22.
+//
+
+import SwiftUI
+
+struct Buttons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Buttons_Previews: PreviewProvider {
+    static var previews: some View {
+        Buttons()
+    }
+}
